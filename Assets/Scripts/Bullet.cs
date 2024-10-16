9cefc4a46872cd0ee7c81194711a741d1f880233
KevinCahill9 <2685130c@student.gla.ac.uk>
2024-10-16 20:05:47 +0100

    Added shooting ability, single fire, burst fire, auto

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("hit" +  collision.gameObject.name + "!");
+            Destroy(gameObject);
+
+        }
+        
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            print("You missed and hit a wall!!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Camera playerCamera;
+
+
+    //Shooting
+    public bool isShooting;
+    public bool readyToShoot;
+    public bool allowReset = true;
+    public float shootingDelay = 2f;
+
+    //Burst
+    public int bulletsPerBurst = 3;
+    public int burstBulletsLeft;
+
+    //Spread
+    public float spreadIntesity;
+
+    //Bullet
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletSpeed = 30;
+    public float bulletPrefabLife = 3f;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentShootingMode == ShootingMode.Auto)
+        {
+            //Holding down left mouse button 
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            //Clicking left mouse button 
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
+            Shoot();
+        }
+
+
+        
+    }
+
+    private void Shoot()
+    {
+
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+        //Bullet is made 
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        bullet.transform.forward = shootingDirection;
+
+
+        //Shoot the bullet 
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletSpeed, ForceMode.Impulse);
+
+        //Bullet life-time ends
+        StartCoroutine(DestroyBulletAfterTime(bullet,bulletPrefabLife));
+
+
+        //Checking if done shooting 
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("Shoot", shootingDelay);
+        }
+
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        return direction + new Vector3(0, 0, 0);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float bulletPrefabLife)
+    {
+        yield return new WaitForSeconds(bulletPrefabLife);
+        Destroy(bullet);
+    }
+}
